4e53490450900b3fb6a836aa5bb978f55d77c397
iklevente <ilyokovacslevente@gmail.com>
2024-12-03 12:56:47 +0100

    code upload

--- XMLTaskNLFUA8/DomParseNLFUA8/src/hu/domparse/nlfua8/DomReadNLFUA8.java
+++ XMLTaskNLFUA8/DomParseNLFUA8/src/hu/domparse/nlfua8/DomReadNLFUA8.java
@@ -5,47 +5,52 @@ import java.io.FileWriter;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class DomReadNLFUA8 {
     public static void main(String[] args) {
+        Document doc = null; // Dokumentum változó a try-catch előtt
         try {
             // XML fájl beolvasása
             File inputFile = new File("XMLNLFUA8.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
+            doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
 
             // Root elem kiírása
             System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
 
-            // Összes gépjármű kiíratása
+            // Fájl író inicializálása
+            FileWriter writer = new FileWriter("OutputRead.txt");
+
+            // Összes gépjármű kiíratása konzolra és fájlba
             NodeList nodeList = doc.getElementsByTagName("Gepjarmu");
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Node node = nodeList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
                     Element element = (Element) node;
-                    System.out.println("Jármű ID: " + element.getAttribute("JarmuID"));
-                    System.out.println("Rendszám: " + element.getElementsByTagName("Rendszam").item(0).getTextContent());
-                    System.out.println("Típus: " + element.getElementsByTagName("JarmuTipus").item(0).getTextContent());
-                    System.out.println("Márka: " + element.getElementsByTagName("Marka").item(0).getTextContent());
-                    System.out.println("Kilométer: " + element.getElementsByTagName("MegtettKilometer").item(0).getTextContent());
-                    System.out.println();
+                    String output = "Jármű ID: " + element.getAttribute("JarmuID") + "\n"
+                            + "Rendszám: " + element.getElementsByTagName("Rendszam").item(0).getTextContent() + "\n"
+                            + "Típus: " + element.getElementsByTagName("JarmuTipus").item(0).getTextContent() + "\n"
+                            + "Márka: " + element.getElementsByTagName("Marka").item(0).getTextContent() + "\n"
+                            + "Kilométer: " + element.getElementsByTagName("MegtettKilometer").item(0).getTextContent() + "\n";
+
+                    // Kiírás a konzolra
+                    System.out.println(output);
+
+                    // Kiírás fájlba
+                    writer.write(output + "\n");
                 }
             }
 
-            // Mentés fájlba
-            FileWriter writer = new FileWriter("OutputRead.txt");
-            writer.write("Az XML tartalmának kiírása blokk formában.\n");
+            // Fájl írásának befejezése
             writer.close();
+            System.out.println("Adatok sikeresen mentve az OutputRead.txt fájlba!");
 
         } catch (Exception e) {
             e.printStackTrace();
